6c62efdf51c0ad842435785f74fbe8ba8b6df622
someda <someda@isenshi.com>
2014-03-05 00:11:44 +0900

    update copyright year

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright 2009 - 2010 Takashi SOMEDA
+# Copyright 2009 - 2014 Takashi SOMEDA
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
